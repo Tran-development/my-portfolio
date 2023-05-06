8d02cd2bd4bee288403cf7cbb5f3e2bed863f93b
Tinh-Tran <70868827+Tran-development@users.noreply.github.com>
2023-05-06 17:41:07 +0700

    up

---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,27 @@
+import Image1 from "../../assets/testimonial1.png"
+import Image2 from "../../assets/testimonial2.png"
+import Image3 from "../../assets/testimonial3.png"
+
+export const Data = [
+    {
+        id: 1, 
+        image: Image1,
+        title: 'Mr.Linh',
+        description: 
+        "Comply with company rules and regulations. Have a good work ethic and a high sense of learning. Always complete the assigned tasks on time."
+    },
+    {
+        id: 2, 
+        image: Image2,
+        title: 'Chirs Evans',
+        description: 
+        "A really good job, all aspects of the project were followed step by step and with good results."
+    },
+    {
+        id: 1, 
+        image: Image3,
+        title: 'Terry John',
+        description: 
+        "A really good job, all aspects of the project were followed step by step and with good results."
+    }
+]
